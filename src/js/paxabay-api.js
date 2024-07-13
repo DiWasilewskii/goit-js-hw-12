77fcfae25ac32b7a19a305534f5607cfa2a64a63
Dmytro Wasilewskii <159699205+DiWasilewskii@users.noreply.github.com>
2024-07-13 13:59:24 +0300

    up

--- src/js/paxabay-api.js
+++ src/js/paxabay-api.js
@@ -1,24 +1,23 @@
-import { handlerError } from './render-function';
-
-const searchSettings = {
-  key: '44443472-3b41bcc651e7d0b56b1888f38',
-  q: '',
-  image_type: 'photo',
-  orientation: 'horizontal',
-  safesearch: true,
-  per_page: 12,
+const params = {
+    key: "44856492-1c75259359e870db5bcbdf4ec",
+    q: "",
+    imageType: "photo",
+    orientation: "horizontal",
+    safesearch: true,
 };
 
-export function generateSearchString(searchText) {
-  searchSettings.q = searchText;
-  const urls = new URLSearchParams(searchSettings);
-  return `https://pixabay.com/api/?${urls}`;
+export function generateHttpsQuery(formValue) {
+    params.q = formValue;
+    const queryString = new URLSearchParams(params).toString();
+    return `https://pixabay.com/api/?${queryString}`;
 }
-export function fetchImage(qveryURL) {
-  return fetch(qveryURL).then(response => {
-    if (!response.ok) {
-      throw new Error(response.status);
-    }
-    return response.json();
-  });
+
+export function fetchPictures(httpsQuery) {
+    return fetch(httpsQuery)
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error(response.status);
+            }
+            return response.json();
+        })
 }
--- src/js/render-function.js
+++ src/js/render-function.js
@@ -1,58 +1,38 @@
-import iziToast from 'izitoast';
-import 'izitoast/dist/css/iziToast.min.css';
-
-iziToast.settings({
-  timeout: 2500,
-  resetOnHover: true,
-  transitionIn: 'flipInX',
-  transitionOut: 'flipOutX',
-  position: 'topRight',
-  titleSize: 25,
-  messageSize: 25,
-  backgroundColor: 'rgba(255, 182, 66, 0.8)',
-});
-
-export const refs = {
-  searchForm: document.querySelector('.js-search-form'),
-  gallery: document.querySelector('.gallery'),
-  loader: document.querySelector('.js-loader'),
-};
-
-export function renderCard(imageData) {
-  return imageData
-    .map(
-      el =>
-        `<li class="card">
-            <a href="${el.largeImageURL}" class="big gallery-link">
-              <img
-                src="${el.previewURL}"
-                alt="${el.tags}"
-                title="${el.tags}"
-                class="card-img"
-            /></a>
-            <ul class="card-title">
-              <li class="card-text-blok">
-                <h2 class="card-title-text">Likes</h2>
-                <p class="card-text-value">${el.likes}</p>
-              </li>
-              <li class="card-text-blok">
-                <h2 class="card-title-text">Views</h2>
-                <p class="card-text-value">${el.views}</p>
-              </li>
-              <li class="card-text-blok">
-                <h2 class="card-title-text">Comments</h2>
-                <p class="card-text-value">${el.comments}</p>
-              </li>
-              <li class="card-text-blok">
-                <h2 class="card-title-text">Downloads</h2>
-                <p class="card-text-value">${el.downloads}</p>
-              </li>
-            </ul>
-          </li>`
-    )
-    .join('');
+export function renderPictures(hits) {
+    return hits.map(({webformatURL, largeImageURL, tags, likes, views, comments, downloads}) => `
+    <li class="gallery-card">
+        <a class="gallery-link" href="${largeImageURL}">
+          <img
+            class="gallery-image"
+            src="${webformatURL}" 
+            alt="${tags}" 
+            width="360"
+            height="152" 
+          />
+        </a>
+        <div class="discribe-box">
+          <ul class="discribe-list">
+            <li class="discribe-item">
+              <h2 class="discribe-title">Likes</h2>
+              <p class="discribe-text">${likes}</p>
+            </li>
+            <li class="discribe-item">
+              <h2 class="discribe-title">Views</h2>
+              <p class="discribe-text">${views}</p>
+            </li>
+            <li class="discribe-item">
+              <h2 class="discribe-title">Comments</h2>
+              <p class="discribe-text">${comments}</p>
+            </li>
+            <li class="discribe-item">
+              <h2 class="discribe-title">Downloads</h2>
+              <p class="discribe-text">${downloads}</p>
+            </li>
+          </ul>
+        </div>
+      </li>
+    `).join("");
 }
-
 export function handlerError(error) {
   switch (error) {
     case 'outdata':
