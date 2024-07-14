c2f9f379164d7fd667e860ea23a6aff982133b5d
Dmytro Wasilewskii <159699205+DiWasilewskii@users.noreply.github.com>
2024-07-14 16:03:59 +0300

    up

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,24 +1,26 @@
 
-const params = {
-    key: "44856492-1c75259359e870db5bcbdf4ec",
-    q: "",
-    imageType: "photo",
-    orientation: "horizontal",
-    safesearch: true,
+import { handlerError } from './render-function';
+
+const searchSettings = {
+  key: '44856492-1c75259359e870db5bcbdf4ec',
+  q: '',
+  image_type: 'photo',
+  orientation: 'horizontal',
+  safesearch: true,
+  per_page: 12,
 };
 
-export function generateHttpsQuery(formValue) {
-    params.q = formValue;
-    const queryString = new URLSearchParams(params).toString();
-    return `https://pixabay.com/api/?${queryString}`;
+export function generateSearchString(searchText) {
+  searchSettings.q = searchText;
+  const urls = new URLSearchParams(searchSettings);
+  return `https://pixabay.com/api/?${urls}`;
+}
+export function fetchImage(qveryURL) {
+  return fetch(qveryURL).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  });
 }
 
-export function fetchPictures(httpsQuery) {
-    return fetch(httpsQuery)
-        .then((response) => {
-            if (!response.ok) {
-                throw new Error(response.status);
-            }
-            return response.json();
-        })
-}
