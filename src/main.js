3f7c5267b3e481ebd00c238d9a28e3eb411bbdcc
Dmytro Wasilewskii <159699205+DiWasilewskii@users.noreply.github.com>
2024-07-17 21:44:27 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -42,25 +42,9 @@ function handlerSubmit(event) {
         .finally(refs.searchForm.reset());
 }
 
-function noImagesError() {
+function fetchError(error) {
     iziToast.error({
         title: "Error",
         message: "Sorry, there are no images matching your search query. Please try again!",
-        position: "center",
-    });
-}
-
-function noRequestError() {
-    iziToast.warning({
-        title: "Caution",
-        message: "Please write your request in the input field!",
-        position: "topRight",
-    });
-}
-
-function endSearchMessage() {
-    iziToast.info({
-        message: "We're sorry, but you've reached the end of search results.",
-        position: "center",
     });
 }
