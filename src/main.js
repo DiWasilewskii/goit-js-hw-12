8ae6e2586768403c51f9e8afc56c24124f990e8b
Dmytro Wasilewskii <159699205+DiWasilewskii@users.noreply.github.com>
2024-07-17 21:35:46 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -42,25 +42,25 @@ function handlerSubmit(event) {
         .finally(refs.searchForm.reset());
 }
 
-function fetchError(error) {
-    switch (error)
-    {
-        case 'outdata':
-      iziToast.warning({
-        title: 'Error',
-        message: 'Введіть данні для пошуку!',
-      });
-        break;
-        
+function noImagesError() {
+    iziToast.error({
+        title: "Error",
+        message: "Sorry, there are no images matching your search query. Please try again!",
+        position: "center",
+    });
+}
 
-    case 'nodata':
-      iziToast.warning({
-        title: 'Error',
-        message:
-          'Sorry, there are no images matching your search query. Please try again!',
-      });
-            break;
-        
-   
-  }
+function noRequestError() {
+    iziToast.warning({
+        title: "Caution",
+        message: "Please write your request in the input field!",
+        position: "topRight",
+    });
+}
+
+function endSearchMessage() {
+    iziToast.info({
+        message: "We're sorry, but you've reached the end of search results.",
+        position: "center",
+    });
 }
